1515b025b61a0967d8e6b5b0877cb562ffa90977
Liou Xia <s224028@dtu.dk>
2025-01-07 10:31:21 +0100

    hi

---
+++ Day2/SOS_Blink.ino
@@ -0,0 +1,53 @@
+
+#define SBLINK 500
+#define LBLINK 1000
+#define WAIT 1000
+
+// the setup function runs once when you press reset or power the board
+void setup()
+{
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+};
+
+// the loop function runs over and over again forever
+void loop()
+{
+  // Making the letter S in Morse
+  for (int i = 0; i < 3; i++)
+  {
+    dot();
+  };
+
+  delay(WAIT);
+
+  // Making the letter O
+  for (int i = 0; i < 3; i++)
+  {
+    dash();
+  };
+
+  delay(WAIT);
+
+  // Making the letter S in Morse
+  for (int i = 0; i < 3; i++)
+  {
+    dot
+  };
+};
+
+void dot()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(SBLINK);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(SBLINK);
+};
+
+void dash()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(LBLINK);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(SBLINK);
+};

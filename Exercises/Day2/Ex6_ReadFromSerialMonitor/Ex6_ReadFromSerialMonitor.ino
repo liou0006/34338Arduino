146fadd62036a49e899e6f01a6f34caa2766c8cc
Liou Xia <s224028@dtu.dk>
2025-01-08 12:25:59 +0100

    finished ex6

--- Exercises/Day2/Ex6_ReadFromSerialMonitor/Ex6_ReadFromSerialMonitor.ino
+++ Exercises/Day2/Ex6_ReadFromSerialMonitor/Ex6_ReadFromSerialMonitor.ino
@@ -1,54 +1,100 @@
 int incomingByte = 0;
+int iB2 = 0;
 
-void setup()
-{
-    Serial.begin(115200);
-    pinMode(2, OUTPUT);
-    pinMode(3, OUTPUT);
-    pinMode(4, OUTPUT);
-    pinMode(5, OUTPUT);
-    pinMode(6, OUTPUT);
+void setup() {
+  Serial.begin(9600);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
 }
 
-void loop()
-{
+void loop() {
 
-    incomingByte = Serial.read();
+  incomingByte = Serial.read();
+  Serial.print("I have received: ");
+  Serial.println((char)incomingByte);
+  delay(1000);
 
-    switch ((char)incomingByte)
-    {
-    case 'a':
-        digitalWrite(2, HIGH);
-        Serial.print("I have received: ");
-        Serial.println((char)incomingByte);
-        break;
-    case 'b':
-        digitalWrite(3, HIGH);
-        Serial.print("I have received: ");
-        Serial.println((char)incomingByte);
-        break;
-    case 'c':
-        digitalWrite(4, HIGH);
-        Serial.print("I have received: ");
-        Serial.println((char)incomingByte);
-        break;
-    case 'd':
-        digitalWrite(5, HIGH);
-        Serial.print("I have received: ");
-        Serial.println((char)incomingByte);
-        break;
-    case 'e':
-        digitalWrite(6, HIGH);
-        Serial.print("I have received: ");
-        Serial.println((char)incomingByte);
-        break;
-    default:
-        digitalWrite(2, LOW);
-        digitalWrite(3, LOW);
-        digitalWrite(4, LOW);
-        digitalWrite(5, LOW);
-        digitalWrite(6, LOW);
-        Serial.println("I have received an invalid character");
-        break;
+  if (incomingByte != -1) {
+    if(incomingByte == 'a' || incomingByte == 'b' || incomingByte == 'c' || incomingByte == 'd' || incomingByte == 'e'){
+      incomingByte = incomingByte;
+    } else {
+      incomingByte = 'Invalid';
     }
+  } else {
+    incomingByte = iB2;
+  }
+  /*else if (incomingByte != 'a' || incomingByte != 'b' || incomingByte != 'c' || incomingByte != 'd' || incomingByte != 'e'){
+    incomingByte = 'Invalid';
+  }*/
+
+  
+  /*if (incomingByte != 'a' || incomingByte != 'b' || incomingByte != 'c' || incomingByte != 'd' || incomingByte != 'e') {
+    Serial.println("I have received an invalid character");
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+    digitalWrite(5, LOW);
+    digitalWrite(6, LOW);
+  }*/
+
+
+  switch (incomingByte) {
+    case 'a':
+      digitalWrite(2, HIGH);
+      digitalWrite(3, LOW);
+      digitalWrite(4, LOW);
+      digitalWrite(5, LOW);
+      digitalWrite(6, LOW);
+      Serial.println("I have received a");
+      delay(1000);
+      break;
+    case 'b':
+      digitalWrite(2, LOW);
+      digitalWrite(3, HIGH);
+      digitalWrite(4, LOW);
+      digitalWrite(5, LOW);
+      digitalWrite(6, LOW);
+      Serial.println("I have received b");
+      delay(1000);
+      break;
+    case 'c':
+      digitalWrite(2, LOW);
+      digitalWrite(3, LOW);
+      digitalWrite(4, HIGH);
+      digitalWrite(5, LOW);
+      digitalWrite(6, LOW);
+      Serial.println("I have received c");
+      delay(1000);
+      break;
+    case 'd':
+      digitalWrite(2, LOW);
+      digitalWrite(3, LOW);
+      digitalWrite(4, LOW);
+      digitalWrite(5, HIGH);
+      digitalWrite(6, LOW);
+      Serial.println("I have received d");
+      break;
+    case 'e':
+      digitalWrite(2, LOW);
+      digitalWrite(3, LOW);
+      digitalWrite(4, LOW);
+      digitalWrite(5, LOW);
+      digitalWrite(6, HIGH);
+      Serial.println("I have received: e");
+      delay(1000);
+      break;
+    case 'Invalid':
+      digitalWrite(2, LOW);
+      digitalWrite(3, LOW);
+      digitalWrite(4, LOW);
+      digitalWrite(5, LOW);
+      digitalWrite(6, LOW);
+      Serial.println("I have received an invalid character");
+      delay(1000);
+      break;
+  }
+  iB2 = incomingByte;
 }

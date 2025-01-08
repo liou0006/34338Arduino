24fddc8e6b1392b0548170ba1a212b22d7dcd8e5
Liou Xia <s224028@dtu.dk>
2025-01-08 11:33:16 +0100

    replaced old file

--- Exercises/Day2/Ex6_ReadFromSerialMonitor/Ex6_ReadFromSerialMonitor.ino
+++ Exercises/Day2/Ex6_ReadFromSerialMonitor/Ex6_ReadFromSerialMonitor.ino
@@ -1,11 +1,43 @@
-void setup(){
-  Serial.begin(9600);
-  char mychar = '4';
-  int val = mychar - '0';
-  mychar = (char) (val + 'A' - 1);
-    Serial.println(mychar);
+char incomingByte = 0;
+
+void setup()
+{
+    Serial.begin(115200);
+    pinMode(2, OUTPUT);
+    pinMode(3, OUTPUT);
+    pinMode(4, OUTPUT);
+    pinMode(5, OUTPUT);
+    pinMode(6, OUTPUT);
 }
 
-void loop(){
-  
+void loop()
+{
+
+    incomingByte = Serial.read();
+
+    switch (incomingByte)
+    {
+    case 'a':
+         digitalWrite(2, HIGH); 
+        break;
+    case 'b':
+         digitalWrite(3, HIGH); 
+        break;
+    case 'c':
+         digitalWrite(4, HIGH); 
+        break;
+    case 'd':
+         digitalWrite(5, HIGH); 
+        break;
+    case 'e':
+         digitalWrite(6, HIGH); 
+        break;
+    default:
+         digitalWrite(2, LOW); 
+         digitalWrite(3, LOW); 
+         digitalWrite(4, LOW); 
+         digitalWrite(5, LOW); 
+         digitalWrite(6, LOW); 
+        break;
+    }
 }

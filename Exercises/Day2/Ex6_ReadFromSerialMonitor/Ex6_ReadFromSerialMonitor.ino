e9d51f0f5d274bddfe6cb4007779eb14a0beed96
Liou Xia <s224028@dtu.dk>
2025-01-08 11:37:29 +0100

    added for default

--- Exercises/Day2/Ex6_ReadFromSerialMonitor/Ex6_ReadFromSerialMonitor.ino
+++ Exercises/Day2/Ex6_ReadFromSerialMonitor/Ex6_ReadFromSerialMonitor.ino
@@ -15,29 +15,40 @@ void loop()
 
     incomingByte = Serial.read();
 
-    switch ((char) incomingByte)
+    switch ((char)incomingByte)
     {
     case 'a':
-         digitalWrite(2, HIGH); 
+        digitalWrite(2, HIGH);
+        Serial.print("I have received: ");
+        Serial.println((char)incomingByte);
         break;
     case 'b':
-         digitalWrite(3, HIGH); 
+        digitalWrite(3, HIGH);
+        Serial.print("I have received: ");
+        Serial.println((char)incomingByte);
         break;
     case 'c':
-         digitalWrite(4, HIGH); 
+        digitalWrite(4, HIGH);
+        Serial.print("I have received: ");
+        Serial.println((char)incomingByte);
         break;
     case 'd':
-         digitalWrite(5, HIGH); 
+        digitalWrite(5, HIGH);
+        Serial.print("I have received: ");
+        Serial.println((char)incomingByte);
         break;
     case 'e':
-         digitalWrite(6, HIGH); 
+        digitalWrite(6, HIGH);
+        Serial.print("I have received: ");
+        Serial.println((char)incomingByte);
         break;
     default:
-         digitalWrite(2, LOW); 
-         digitalWrite(3, LOW); 
-         digitalWrite(4, LOW); 
-         digitalWrite(5, LOW); 
-         digitalWrite(6, LOW); 
+        digitalWrite(2, LOW);
+        digitalWrite(3, LOW);
+        digitalWrite(4, LOW);
+        digitalWrite(5, LOW);
+        digitalWrite(6, LOW);
+        Serial.println("I have received an invalid character");
         break;
     }
 }
